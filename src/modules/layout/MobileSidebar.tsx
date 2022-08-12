56a3aa33549cd6ed2377df4080c65a6089a81ab4
Caleb Rivera <calebrossr@gmail.com>
2022-08-12 00:48:22 -0400

    Add close button to mobile sidebar

--- src/modules/layout/MobileSidebar.tsx
+++ src/modules/layout/MobileSidebar.tsx
@@ -1,6 +1,8 @@
 import { Dialog, Transition } from "@headlessui/react";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect } from "react";
+import { useMediaQuery } from "react-responsive";
+import XIcon from "./XIcon";
 
 interface Props {
   show: boolean;
@@ -8,9 +10,17 @@ interface Props {
 }
 
 const MobileSidebar: React.FC<Props> = ({ show, close }) => {
+  const isMd = useMediaQuery({
+    query: "(min-width: 768px)",
+  });
+
+  useEffect(() => {
+    if (isMd) close();
+  }, [isMd, close]);
+
   return (
     <Transition show={show} as={React.Fragment}>
-      <Dialog as="div" className="relative md:hidden" onClose={() => close()}>
+      <Dialog as="div" className="relative z-20" onClose={() => close()}>
         <Transition.Child
           enter="transition-opacity ease-in-out duration-300"
           enterFrom="opacity-0"
@@ -34,6 +44,9 @@ const MobileSidebar: React.FC<Props> = ({ show, close }) => {
             as={React.Fragment}
           >
             <Dialog.Panel className="fixed right-0 flex h-screen w-full max-w-sm items-center justify-center bg-gray-900">
+              <button className="fixed top-8 right-8" onClick={() => close()}>
+                <XIcon />
+              </button>
               <ul className="flex max-h-full flex-col gap-16 text-3xl">
                 <li>
                   <Link href="#about">
@@ -68,6 +81,7 @@ const MobileSidebar: React.FC<Props> = ({ show, close }) => {
                 <li>
                   <Link href="https://github.com/LightBounded">
                     <a
+                      target="_blank"
                       className="underline underline-offset-8"
                       onClick={() => close()}
                     >
@@ -78,6 +92,7 @@ const MobileSidebar: React.FC<Props> = ({ show, close }) => {
                 <li>
                   <Link href="https://www.linkedin.com/in/caleb-rivera-405658234/">
                     <a
+                      target="_blank"
                       className="underline underline-offset-8"
                       onClick={() => close()}
                     >
--- src/modules/layout/XIcon.tsx
+++ src/modules/layout/XIcon.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-const MenuIcon = () => {
+const XIcon = () => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      className="h-7 w-7"
+      className="h-8 w-8"
       fill="none"
       viewBox="0 0 24 24"
       stroke="currentColor"
@@ -13,10 +13,10 @@ const MenuIcon = () => {
       <path
         strokeLinecap="round"
         strokeLinejoin="round"
-        d="M4 6h16M4 12h16M4 18h16"
+        d="M6 18L18 6M6 6l12 12"
       />
     </svg>
   );
 };
 
-export default MenuIcon;
+export default XIcon;
