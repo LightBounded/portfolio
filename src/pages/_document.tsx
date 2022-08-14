1901e216ca965018eb467dd2325d7cda6542eca9
Caleb Rivera <calebrossr@gmail.com>
2022-08-14 19:05:46 -0400

    Convert favicon to icon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" type="image/png" href="/favicon.png" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <body>
         <Main />
